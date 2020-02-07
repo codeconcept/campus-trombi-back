a55ce4f072e38c22655ad30c5c88afb60840bc0b
codeconcept <contact@codeconcept.fr>
2020-02-07 17:44:53 +0100

    Body parser + register route

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 app.get("/favicon.ico", (req, res) => {
   res.status(204);
 });
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello server" });
+});
+
+app.post("/register", (req, res) => {
+  console.log("body", req.body);
+  res.send("todo register");
 });
 
 const PORT = 3001;
